7946e6828c49540ba2654fb32b843067b5f289cc
Joel Bartlett <joel@murbar.com>
2019-02-22 11:47:31 -0500

    Check types on Tab props

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -20,3 +21,7 @@ const Tab = props => {
 // Make sure you include PropTypes on your props.
 
 export default Tab;
+
+Tab.propTypes = {
+  tab: PropTypes.string
+};
