625fd0fa6c4537a82953a7f432969225caf7b077
Joel Bartlett <joel@murbar.com>
2019-02-22 11:46:11 -0500

    Build out Tabs

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Tab from './Tab';
-const Tabs = props => {
+
+const Tabs = ({ tabs }) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {tabs.map(t => (
+          <Tab tab={t} key={t} />
+        ))}
       </div>
     </div>
   );
@@ -14,3 +19,7 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+};
