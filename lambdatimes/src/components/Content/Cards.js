a90999e8963108baa151be3ef4ab2446130edb8a
Joel Bartlett <joel@murbar.com>
2019-02-22 12:02:07 -0500

    Check types for Cards props

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 const Cards = props => {
@@ -7,9 +8,16 @@ const Cards = props => {
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
+
+Cards.propTypes = {
+  headline: PropTypes.string,
+  tab: PropTypes.string,
+  img: PropTypes.string,
+  author: PropTypes.string
+};
