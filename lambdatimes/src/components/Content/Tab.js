8c9290bce5f0c8e3918e84de829d395a4745a6ac
Joel Bartlett <joel@murbar.com>
2019-02-22 11:59:27 -0500

    Build out Tab

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Tab = props => {
+const Tab = ({ tab, selectedTab, selectTabHandler }) => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  const tabClasses = tab === selectedTab ? 'tab active-tab' : 'tab';
   return (
     <div
-      className={''}
+      className={tabClasses}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+        selectTabHandler(tab);
       }}
     >
-      {props.tab.toUpperCase()}
+      {tab.toUpperCase()}
     </div>
   );
 };
@@ -23,5 +25,7 @@ const Tab = props => {
 export default Tab;
 
 Tab.propTypes = {
-  tab: PropTypes.string
+  tab: PropTypes.string,
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func
 };
