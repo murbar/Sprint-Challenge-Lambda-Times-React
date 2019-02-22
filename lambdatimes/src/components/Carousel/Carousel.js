82844d7458b3f9857fc02b1dad93f96139fb2b3d
Joel Bartlett <joel@murbar.com>
2019-02-22 13:14:28 -0500

    Build out Carousel w/infinite scroll - Stretch 2 complete

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import { carouselData } from '../../data';
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
-    }
+      imgSrcs: [],
+      currentImgIndex: 0
+    };
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.setState({
+      imgSrcs: carouselData
+    });
   }
 
   leftClick = () => {
-
-  }
+    let newIndex = this.state.currentImgIndex;
+    newIndex--;
+    const lastImgIndex = this.state.imgSrcs.length - 1;
+    if (newIndex < 0) {
+      newIndex = lastImgIndex;
+    }
+    this.setState({ currentImgIndex: newIndex });
+  };
 
   rightClick = () => {
-
-  }
+    let newIndex = this.state.currentImgIndex;
+    newIndex++;
+    const lastImgIndex = this.state.imgSrcs.length - 1;
+    if (newIndex > lastImgIndex) {
+      newIndex = 0;
+    }
+    this.setState({ currentImgIndex: newIndex });
+  };
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+    const { imgSrcs, currentImgIndex: i } = this.state;
+    return <img src={imgSrcs[i]} style={{ display: 'block' }} alt="Carousel" />;
+  };
+
+  render() {
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        {this.selectedImage()}
+        <div className="left-button" onClick={this.leftClick}>
+          {'<'}
+        </div>
+        <div className="right-button" onClick={this.rightClick}>
+          {'>'}
+        </div>
       </div>
-    )
+    );
   }
 }
